a40ba73c00ae56072d38dfdd853f70592c5fd0c8
Ionut Milica <ionut.milica@gmail.com>
2017-12-02 20:34:15 +0200

    Added license

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Webshot
+
+Simple toy service that generates thumbnails and collects metadata
+from various sites via a rest api.
+
+## Requirements
+- Node >= 8.8.1
+- Yarn >= 1.3.1
--- src/routes/screenshot/index.ts
+++ src/routes/screenshot/index.ts
@@ -9,6 +9,10 @@ export default ({ webShot }: Container) => {
       return res.status(422).send({ message: 'An url attribute is expected.' });
     }
 
-    res.status(200).send(await webShot.process(url));
+    try {
+      res.status(200).send(await webShot.process(url));
+    } catch {
+      res.status(404).send({ message: 'Site not found.' });
+    }
   };
 };
